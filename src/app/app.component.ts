d07161d34323c3e803beb4347f723e66b5b569ac
Andre Richardson <andrerichards@Sridevi-PC.localdomain>
2019-09-23 16:28:57 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  // styleUrls: ['./app.component.scss']
+  styles: [
+    `
+      h3 {
+        color: dodgerblue;
+      }
+    `
+  ]
 })
-export class AppComponent {
-  title = 'first-app';
-}
+export class AppComponent {}
